9085e10d4801dcd3bf6803576fbbd0385e42776a
eduardo <bhjkmufdtujh@gmail.com>
2024-10-17 11:27:54 -0300

    alteracoes

--- teste/src/components/Header/index.jsx
+++ teste/src/components/Header/index.jsx
@@ -1,24 +1,25 @@
 import styles from './Header.module.css'
 import img from  '../Header/img/logo-header.svg'
 import cart from '../Header/img/mini-cart.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Header(){
     return (
       <header className={styles.header}>
            
-            <div className={styles.h11}>
+            <div className={styles.hea_1}>
              
               <img src ={img}  className={styles.header_img}  alt='IMG'></img>  
               <input type='text' placeholder='Pesquisar produto...'></input>
-                <label className={styles.headerbutton}>Cadastrar</label>
+                <label className={styles.headerbutton}>Cadastra-se</label>
                 <label className={styles.headerbutton2}>Entrar</label >
                 <div className={styles.div_cart}>
                 <img  src={cart}  className={styles.cart}  alt='carrinho'></img>
                 </div>
            
             </div>
-            <div className={styles.h22}>
+            <div className={styles.hea_2}>
               <nav>
             <a href=''>Home</a>
             <a href=''>Produtos</a>
